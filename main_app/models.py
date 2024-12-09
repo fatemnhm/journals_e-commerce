99682f0fb9035f8bb3c3c89fc6474cfb09c6c0ac
fatemnhm <fatemanhm@gmail.com>
2024-12-09 11:05:21 +0300

    initail

---
+++ main_app/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+import datetime
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(default=0.0, decimal_places=2, max_digits=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
+    description = models.TextField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+    image = models.ImageField(upload_to='products/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0.0, decimal_places=2, max_digits=6)
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    address = models.TextField(max_length=250)
+    phone = models.CharField(max_length=10)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
+
+
