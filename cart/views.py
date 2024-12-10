eee0121d7569ab74aa7547a18b95ded992e9e7b5
fatemnhm <fatemanhm@gmail.com>
2024-12-10 12:31:00 +0300

    cart

--- cart/views.py
+++ cart/views.py
@@ -10,7 +10,21 @@ def cart_summary(request):
     return render(request, 'cart_summary.html')
 
 def cart_add(request):
-    p
+    #get cart from session
+    cart = Cart(request)
+    #if request is post 
+    if request.POST.get('action') == 'post':
+        #get product_id from the request
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'product': product.name})
+        return response
+        
+
+   
+       
+
 
 def cart_delete(request):
     pass
